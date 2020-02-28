12b36232fb243bd4499f093897247b56304f7dd4
Will Smith <wismith@davidson.edu>
2020-02-28 00:08:04 -0800

    filter database.json

--- index.js
+++ index.js
@@ -56,6 +56,70 @@ let fakeYelpAPI = () => {
   return list;
 };
 
+// Reference Dictionary for Zip Codes / Neighborhoods
+let zipRef = {
+  "Hayes Valley": "94102",
+  "Tenderloin": "94102",
+  "North of Market": "94102",
+  "SoMa": "94103",
+  "Financial District": "94104",
+  "Embarcadero South": "94105",
+  "Portrero Hill": "94107",
+  "Chinatown": "94108",
+  "Nob Hill": "94109",
+  "Mission District": "94110",
+  "Embarcadero North": "94111",
+  "Ingelside-Excelsior/Crocker-Amazon": "94112",
+  "Castro/Noe Valley": "94114",
+  "Western Addition/Japantown/Pacific Heights": "94115",
+  "Outer Sunset": "94116",
+  "Haight-Ashbury": "94117",
+  "Inner Richmond": "94118",
+  "Outer Richmond": "94121",
+  "Sunset": "94122",
+  "Marina": "94123",
+  "Bayview-Hunters Point": "94124",
+  "St. Francis Wood/Miraloma/West Portal": "94127",
+  "Presidio": "94129",
+  "Twin Peaks-Glen Park": "94131",
+  "Lake Merced": "94132",
+  "North Beach": "94133",
+  "Walnut Creek": "94595",
+  "Oakland": "94601"
+}
+
+// Function to check restaurants with Yelp data format with input preferences
+let filterCheck = (restaurant, preferences) => {
+  let checksOut = true;
+  if (preferences.hasOwnProperty('neighborhood')) {
+    if (restaurant.location.zip_code !== zipRef[preferences.neighborhood]) {
+      checksOut = false;
+    }
+  }
+  if (preferences.hasOwnProperty('price')) {
+    if (restaurant.price !== preferences.price) {
+      checksOut = false;
+    }
+  }
+  if (preferences.hasOwnProperty('cuisine')) {
+    let restaurantCategories = [];
+    for (let category of restaurant.categories) {
+      restaurantCategories.push(category.title);
+    }
+    if (!restaurantCategories.includes(preferences.cuisine)) {
+      checksOut = false;
+    }
+  }
+  return checksOut;
+}
+
+let getYelpData = (jsonFile) => {
+  let text = fs.readFileSync(jsonFile, 'utf-8');
+  let yelpRestaurants = JSON.parse(text);
+  console.log(Object.keys(yelpRestaurants));
+  return yelpRestaurants.businesses;
+}
+
 
 app.get('/search', (req, res) => {
   // Serves the body of the page to the container
@@ -87,7 +151,18 @@ app.get('/results', (req, res) => {
   console.log(preferences);
   console.log(preferences.hasOwnProperty('neighborhood'));
 
-  let results = fakeYelpAPI().filter(restaurant => {
+  let yelpRestaurants = getYelpData('database.json');
+
+  console.log(Object.keys(yelpRestaurants));
+  console.log('end of object keys');
+  console.log(typeof yelpRestaurants);
+
+  let results = yelpRestaurants.filter(restaurant => filterCheck(restaurant, preferences));
+
+  results = results.sort((a,b) => b.rating - a.rating);
+
+
+  /* let results = fakeYelpAPI().filter(restaurant => {
     let checksOut = true;
     for (let preference in preferences) {
       if (preferences.hasOwnProperty(preference)) {
@@ -97,7 +172,7 @@ app.get('/results', (req, res) => {
       }
     }
     return checksOut;
-  })
+  }) */
 
   let format = {
     layout: 'index',
